4026ceb831b15ee6d5e335dcba943f18d81be32c
Ben Papp <contact@benpapp.com>
2025-07-16 10:16:23 -0400

    - Updating servers.json to be fully correct
    - Added favicon and logo

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -5,6 +5,12 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className='flex flex-col items-center mb-4'>
+      <a href="https://remc1.net" target="_blank" rel="noopener noreferrer">
+        <img src="/src/assets/REMC1.png" alt="REMC1 Logo. Click to go to REMC1.net" />
+      </a>
+      </div>
+      <App />
+
   </StrictMode>,
 )
