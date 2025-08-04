17f0686fbcfd73b483e1bb5f18ac39aa4278dc1f
Ben Papp <contact@benpapp.com>
2025-08-04 09:47:19 -0400

    Update .gitignore to include .pem files and enhance Dashboard functionality by ensuring selected server state is managed correctly

--- app/src/pages/Dashboard.tsx
+++ app/src/pages/Dashboard.tsx
@@ -391,9 +391,11 @@ const confirmDelete = async () => {
                           id="server-select"
                           value={selectedServer.host}
                           onChange={(e) => {
-                            setSelectedServer(servers.find(server => server.host === e.target.value) || {} as Server)
+                            const newServer = servers.find(server => server.host === e.target.value);
+                            setSelectedServer(newServer || {} as Server);
                             setServiceStatus('inactive');
                             setIsLoggedIn(false);
+                            setToken(null);
                           }}
                           className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                         >
